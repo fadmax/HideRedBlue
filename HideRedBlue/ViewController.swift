4a567780bed227f40e01e89ba2b3ce2c75b5a502
fadmax <fadmax@msn.com>
2016-01-23 17:48:31 +0300

    adding files

--- HideRedBlue/ViewController.swift
+++ HideRedBlue/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueCar: UIImageView!
+    @IBOutlet weak var redCar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func hideRed(sender: AnyObject) {
+        redCar.hidden = true
+    }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueCar.hidden = true
+    }
 
 }
 
